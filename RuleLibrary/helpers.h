bf4a908eb96b5452fea0418934f5652cada0788f
newenclave <pikrat.acid@gmail.com>
2019-08-27 15:45:35 +0300

    Init

--- RuleLibrary/helpers.h
+++ RuleLibrary/helpers.h
@@ -22,6 +22,12 @@ namespace helpers {
 			return false;
 		}
 
+		template <typename CharT>
+		static bool is_gap(CharT c)
+		{
+			return c == '_';
+		}
+
 		template <typename Itr>
 		static Itr skip_spaces(Itr begin, Itr end)
 		{
@@ -43,8 +49,8 @@ namespace helpers {
 		}
 
 		template <typename ItrT, typename ContainerT>
-		static auto read_string(ItrT begin, ItrT end, const ContainerT &stop) 
-			-> std::basic_string<typename ItrT::value_type>
+		static std::basic_string<typename ItrT::value_type> 
+			read_string(ItrT begin, ItrT end, const ContainerT &stop)
 		{
 			using string_type = std::basic_string<typename ItrT::value_type>;
 			string_type res;
@@ -96,6 +102,110 @@ namespace helpers {
 				++begin;
 			}
 			return res;
+		}
+
+		template <typename CharT>
+		static bool valid_for_dec(CharT c)
+		{
+			return ('0' <= c) && (c <= '9' );
+		}
+
+		template <typename CharT>
+		static bool valid_for_dec_(CharT c)
+		{
+			return valid_for_dec(c) || is_gap(c);
+		}
+
+		template <typename ItrT>
+		static bool check_if_float(ItrT begin, ItrT end)
+		{
+			bool float_found = false;
+			while(begin != end && !float_found) {
+				char c = *begin++;
+				if((c == 'e') || (c == 'E')) {
+					float_found = true;
+				} else if( c == '.' ) {
+					if((begin != end) && (*begin == '.')) {
+						return false;
+					} else {
+						float_found = true;
+					}
+				} else if(!valid_for_dec_(c)) {
+					return false;
+				}
+			}
+			return float_found;
+		}
+
+		template <typename ContT>
+		static double parse_float(const ContT &cont)
+		{
+			auto bb = std::begin(cont);
+			return parse_float(bb, std::end(cont));
+		}
+
+		template <typename ItrT>
+		static double parse_float(ItrT &s, ItrT end)
+		{
+			double a = 0.0;
+			int e = 0;
+			int c = 0;
+
+			while((s != end) && valid_for_dec_(c = *s++)) {
+				if(!is_gap(c)) {
+					a = a * 10.0 + (c - '0');
+				}
+			}
+
+			if(c == '.') {
+				while((s != end) && valid_for_dec_(c = *s++)) {
+					if(!is_gap(c)) {
+						a = a*10.0 + (c - '0');
+						e = e-1;
+					}
+				}
+			}
+
+			if(c == 'e' || c == 'E') {
+				int sign = 1;
+				int i = 0;
+				c = *s++;
+
+				if (c == '+') {
+					c = *s++;
+				} else if (c == '-') {
+					c = *s++;
+					sign = -1;
+				}
+
+				while(valid_for_dec_(c)) {
+					if(is_gap(c)) {
+						c = *s++;
+					}
+					i = i * 10 + (c - '0');
+					if(s == end) {
+						break;
+					} else {
+						c = *s++;
+					}
+				}
+				e += i*sign;
+			}
+
+			while(e > 0) {
+				a *= 10.0;
+				e--;
+			}
+
+			while(e < 0) {
+				a *= 0.1;
+				e++;
+			}
+
+			if(s != end) {
+				--s;
+			}
+			return a;
 		}
 	};
 }
