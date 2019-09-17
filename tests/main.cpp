96452a62fae78a48af492d594db1796345a9c1f2
newenclave <pikrat.acid@gmail.com>
2019-09-17 16:14:20 +0300

    v2

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,5 @@
-#include "erules/common/parser_base.h"
 #include "erules/rules/ast.h"
+#include "erules/rules/parser.h"
 #include <iostream>
 
 using namespace erules::common;
@@ -9,12 +9,19 @@ struct node {
     using uptr = std::unique_ptr<node>;
 };
 
-using parser = parser_base<node::uptr, char, std::string>;
-using lexer = parser::lexer_type;
+using parser_type = parser<char, std::string>;
+using lexer_type = typename parser_type::lexer_type;
 
 int main()
 {
-    lexer lex;
+    lexer_type lex;
+    std::string test1 = "test \"213 123\" 12.90 8987";
+    lex.set_ident_key("IDENT");
+    lex.set_string_key("STRING");
+    lex.set_number_key("NUMBER");
+    lex.set_float_key("FLOAT");
+    lex.reset(test1);
+    auto vals = lex.run();
 
     return 0;
 }
